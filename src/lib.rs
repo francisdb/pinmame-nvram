6b872e0b200a9532b8d8cc2e302dad3dd460aafd
Francis De Brabandere <francisdb@gmail.com>
2024-11-22 18:13:06 +0100

    Initial commit, wip

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+mod model;
+
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
+
+pub fn read() {
+    // open ~/vpinball/tables/Medieval Madness (Williams 1997)/pinmame/roms/mm_109c.zip
+    let mut nvram_file = File::open(
+        "/home/francisdb/vpinball/tables/Medieval Madness (Williams 1997)/pinmame/nvram/mm_109c.nv",
+    )
+    .unwrap();
+    println!("nvram_file: {:?}", nvram_file);
+
+    // seek to first high score name
+    // read high score name
+
+    let first_initials = read_initials(&mut nvram_file, 0x1D29);
+    println!("First place initials: {:?}", first_initials);
+    let second_initials = read_initials(&mut nvram_file, 0x1D31);
+    println!("Second place initials: {:?}", second_initials);
+    let third_initials = read_initials(&mut nvram_file, 0x1D39);
+    println!("Third place initials: {:?}", third_initials);
+    let fourth_initials = read_initials(&mut nvram_file, 0x1D41);
+    println!("Fourth place initials: {:?}", fourth_initials);
+}
+
+fn read_initials(nvram_file: &mut File, location: u64) -> String {
+    nvram_file.seek(SeekFrom::Start(location)).unwrap();
+    let mut buff = [0; 3];
+    nvram_file.read(&mut buff).unwrap();
+    // TODO is utf8 the right encoding? I would expect ASCII which is a subset of UTF8
+    std::str::from_utf8(&buff).unwrap().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        read();
+        assert_eq!(5, 5);
+    }
+}
---
+++ src/model.rs
@@ -0,0 +1,316 @@
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Serialize, Deserialize)]
+struct Checksum16 {
+    pub start: String,
+    pub end: String,
+    pub label: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Checksum8 {
+    pub start: String,
+    pub end: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub groupings: Option<u64>,
+    pub label: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Adjustment {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub _note: Option<String>,
+    pub label: String,
+    pub start: Start,
+    pub encoding: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub default: Option<StringOrNumber>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub values: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub min: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub multiple_of: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub length: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub suffix: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub special_values: Option<HashMap<String, String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nibble: Option<Nibble>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scale: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub offset: Option<u64>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Audit {
+    pub label: String,
+    pub start: String,
+    pub encoding: String,
+    pub length: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub units: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub suffix: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scale: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nibble: Option<Nibble>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Score {
+    pub start: Start,
+    pub encoding: String,
+    pub length: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub suffix: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nibble: Option<Nibble>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scale: Option<i64>,
+}
+
+// TODO is this the same as HighScore?
+#[derive(Serialize, Deserialize)]
+struct ModeChampion {
+    pub label: String,
+    pub short_label: String,
+    pub initials: Initials,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub score: Option<Score>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub timestamp: Option<LastGame>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub _note: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct HighScore {
+    pub label: String,
+    pub short_label: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub initials: Option<Initials>,
+    pub score: Score,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum Start {
+    Hex(HexString),
+    Integer(i64),
+}
+
+#[derive(Serialize, Deserialize)]
+struct LastGame {
+    pub start: Start,
+    pub encoding: String,
+    pub length: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nibble: Option<Nibble>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Nibble {
+    High,
+    Low,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Endian {
+    Big,
+    Little,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Initials {
+    pub start: Start,
+    pub encoding: String,
+    pub length: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub default: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nibble: Option<Nibble>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub mask: Option<HexString>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum Notes {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum Adjustments {
+    Anonymous(Vec<Adjustment>),
+    Named(HashMap<String, Adjustment>),
+}
+
+#[derive(Debug)]
+struct HexString {
+    pub value: u64,
+    pub serialized: String,
+}
+
+impl Serialize for HexString {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.serialized.as_str())
+    }
+}
+
+impl<'de> Deserialize<'de> for HexString {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        if s.starts_with("0x") && s[2..].chars().all(|c| c.is_digit(16)) {
+            let value = u64::from_str_radix(&s[2..], 16).unwrap();
+            Ok(HexString {
+                value,
+                serialized: s,
+            })
+        } else {
+            Err(serde::de::Error::custom("invalid hex string"))
+        }
+    }
+}
+
+impl fmt::Display for HexString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({})", self.serialized, self.value)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct State {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    _note: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    _note2: Option<String>,
+    encoding: String,
+    label: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    short_label: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    length: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    nibble: Option<Nibble>,
+    start: Start,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    mask: Option<HexString>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    endian: Option<Endian>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    scale: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    suffix: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    values: Option<Vec<u64>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    offset: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    special_values: Option<HashMap<String, String>>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum StateOrStateList {
+    State(State),
+    StateList(Vec<State>),
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum StringOrNumber {
+    String(String),
+    Number(u64),
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum AuditOrNote {
+    Audit(Audit),
+    Note(String),
+}
+
+#[derive(Serialize, Deserialize)]
+struct NvramMap {
+    pub _notes: Notes,
+    pub _copyright: String,
+    pub _license: String,
+    pub _endian: String,
+    pub _roms: Vec<String>,
+    pub _fileformat: f64,
+    pub _version: f64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub _ramsize: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub _game: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub last_played: Option<LastGame>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub last_game: Option<Vec<LastGame>>,
+    pub high_scores: Vec<HighScore>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub mode_champions: Option<Vec<ModeChampion>>,
+    // keys are nomrally numbers except for notes, which as value are strings
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub audits: Option<HashMap<String, HashMap<String, AuditOrNote>>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub adjustments: Option<HashMap<String, Adjustments>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub checksum8: Option<Vec<Checksum8>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub checksum16: Option<Vec<Checksum16>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub limits: Option<HashMap<String, String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub game_state: Option<HashMap<String, StateOrStateList>>,
+}
+
+// test: read file write file and compare
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use serde_json::Value;
+
+    #[test]
+    fn read_all_nvram_maps() {
+        // read all ../pinmame-nvram-maps/*.json
+        for file in std::fs::read_dir("../pinmame-nvram-maps").unwrap() {
+            let file = file.unwrap();
+            let path = file.path();
+            let file_name = path.file_name().unwrap().to_str().unwrap().to_owned();
+            if file_name.ends_with(".nv.json") {
+                println!("Reading {}", file_name);
+                let json = std::fs::read_to_string(path).unwrap();
+                let nvram_map: NvramMap = serde_json::from_str(&json).unwrap();
+                let json2 = serde_json::to_string_pretty(&nvram_map).unwrap();
+
+                // read json as Value to compare without formatting
+                let json_obj: Value = serde_json::from_str(&json).unwrap();
+                let json_obj2: Value = serde_json::from_str(&json2).unwrap();
+
+                assert_eq!(json_obj, json_obj2, "Failed for {}", file_name);
+            }
+        }
+    }
+}
