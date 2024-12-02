7f663fd4c5f15bb23ad26cced9ed9710a7fc77ff
Francis De Brabandere <francisdb@gmail.com>
2024-12-02 21:34:28 +0100

    test: enable st_162h

--- tests/nv_st_162h.rs
+++ tests/nv_st_162h.rs
@@ -4,14 +4,15 @@ use std::io;
 use std::path::Path;
 
 #[test]
-#[ignore = "Only st_162h.nv is available and incompatible"]
 fn test_star_trek_stern() -> io::Result<()> {
-    let mut nvram = Nvram::open(Path::new("testdata/st_161h.nv"))?.unwrap();
+    // TODO add test for st_161h.nv
+    // TODO fix last highscore value when this is implemented: https://github.com/tomlogic/pinmame-nvram-maps/issues/34
+    let mut nvram = Nvram::open(Path::new("testdata/st_162h.nv"))?.unwrap();
 
     let last_game = nvram.read_last_game()?;
     let expected = Vec::from([
         LastGamePlayer {
-            score: 691_690,
+            score: 0,
             label: None,
         },
         LastGamePlayer {
@@ -34,32 +35,32 @@ fn test_star_trek_stern() -> io::Result<()> {
         HighScore {
             label: Some("Grand Champion".to_string()),
             short_label: Some("GC".to_string()),
-            initials: "PML".to_string(),
-            score: 50_000_000,
+            initials: "SSR".to_string(),
+            score: 75_000_000,
         },
         HighScore {
-            label: Some("1st".to_string()),
+            label: Some("#1".to_string()),
             short_label: Some("#1".to_string()),
-            initials: "LNK".to_string(),
-            score: 31_000_000,
+            initials: "LON".to_string(),
+            score: 55_000_000,
         },
         HighScore {
-            label: Some("2nd".to_string()),
+            label: Some("#2".to_string()),
             short_label: Some("#2".to_string()),
-            initials: "JRK".to_string(),
-            score: 25_000_000,
+            initials: "GGF".to_string(),
+            score: 40_000_000,
         },
         HighScore {
-            label: Some("3rd".to_string()),
+            label: Some("#3".to_string()),
             short_label: Some("#3".to_string()),
-            initials: "J Y".to_string(),
-            score: 22_000_000,
+            initials: "JMR".to_string(),
+            score: 30_000_000,
         },
         HighScore {
-            label: Some("4th".to_string()),
+            label: Some("#4".to_string()),
             short_label: Some("#4".to_string()),
-            initials: "C G".to_string(),
-            score: 18_000_000,
+            initials: "T\0".to_string(),
+            score: 25_000_000,
         },
     ]);
     assert_eq!(expected, scores);
