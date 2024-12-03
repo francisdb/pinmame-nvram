96e6224106a77addbb58e93e40f29edc60b9d278
Francis De Brabandere <francisdb@gmail.com>
2024-12-04 00:14:36 +0100

    test: funhouse

--- tests/nv_fh_905h.rs
+++ tests/nv_fh_905h.rs
@@ -3,20 +3,81 @@ use pretty_assertions::assert_eq;
 use std::io;
 use std::path::Path;
 
-// TODO create files for fh_905h.nv and fh_906h.nv
-
 #[test]
-fn test_funhouse() -> io::Result<()> {
-    let mut nvram = Nvram::open(Path::new("testdata/fh_l9.nv"))?.unwrap();
+fn test_funhouse_905h() -> io::Result<()> {
+    let mut nvram = Nvram::open(Path::new("testdata/fh_905h.nv"))?.unwrap();
 
     let last_game = nvram.read_last_game()?;
     let expected = Vec::from([
         LastGamePlayer {
-            score: 743_820,
+            score: 756_330,
+            label: None,
+        },
+        LastGamePlayer {
+            score: 605_990,
+            label: None,
+        },
+        LastGamePlayer {
+            score: 1_014_990,
             label: None,
         },
         LastGamePlayer {
             score: 0,
+            label: None,
+        },
+    ]);
+    assert_eq!(Some(expected), last_game);
+
+    let scores = nvram.read_highscores()?;
+    let expected = Vec::from([
+        HighScore {
+            label: Some("Grand Champion".to_string()),
+            short_label: Some("GC".to_string()),
+            initials: "LED".to_string(),
+            score: 15_000_000,
+        },
+        HighScore {
+            label: Some("First Place".to_string()),
+            short_label: Some("1st".to_string()),
+            initials: "PML".to_string(),
+            score: 12_000_000,
+        },
+        HighScore {
+            label: Some("Second Place".to_string()),
+            short_label: Some("2nd".to_string()),
+            initials: "EAE".to_string(),
+            score: 11_000_000,
+        },
+        HighScore {
+            label: Some("Third Place".to_string()),
+            short_label: Some("3rd".to_string()),
+            initials: "J K".to_string(),
+            score: 10_000_000,
+        },
+        HighScore {
+            label: Some("Fourth Place".to_string()),
+            short_label: Some("4th".to_string()),
+            initials: "HEY".to_string(),
+            score: 9_000_000,
+        },
+    ]);
+    assert_eq!(expected, scores);
+
+    Ok(())
+}
+
+#[test]
+fn test_funhouse_906h() -> io::Result<()> {
+    let mut nvram = Nvram::open(Path::new("testdata/fh_906h.nv"))?.unwrap();
+
+    let last_game = nvram.read_last_game()?;
+    let expected = Vec::from([
+        LastGamePlayer {
+            score: 2_170_790,
+            label: None,
+        },
+        LastGamePlayer {
+            score: 1_880_850,
             label: None,
         },
         LastGamePlayer {
@@ -47,7 +108,7 @@ fn test_funhouse() -> io::Result<()> {
         HighScore {
             label: Some("Second Place".to_string()),
             short_label: Some("2nd".to_string()),
-            initials: "MJC".to_string(),
+            initials: "EAE".to_string(),
             score: 11_000_000,
         },
         HighScore {
---
+++ tests/nv_fh_l9.rs
@@ -0,0 +1,69 @@
+use pinmame_nvram::{HighScore, LastGamePlayer, Nvram};
+use pretty_assertions::assert_eq;
+use std::io;
+use std::path::Path;
+
+// TODO create files for fh_905h.nv and fh_906h.nv
+
+#[test]
+fn test_funhouse() -> io::Result<()> {
+    let mut nvram = Nvram::open(Path::new("testdata/fh_l9.nv"))?.unwrap();
+
+    let last_game = nvram.read_last_game()?;
+    let expected = Vec::from([
+        LastGamePlayer {
+            score: 743_820,
+            label: None,
+        },
+        LastGamePlayer {
+            score: 0,
+            label: None,
+        },
+        LastGamePlayer {
+            score: 0,
+            label: None,
+        },
+        LastGamePlayer {
+            score: 0,
+            label: None,
+        },
+    ]);
+    assert_eq!(Some(expected), last_game);
+
+    let scores = nvram.read_highscores()?;
+    let expected = Vec::from([
+        HighScore {
+            label: Some("Grand Champion".to_string()),
+            short_label: Some("GC".to_string()),
+            initials: "LED".to_string(),
+            score: 15_000_000,
+        },
+        HighScore {
+            label: Some("First Place".to_string()),
+            short_label: Some("1st".to_string()),
+            initials: "PML".to_string(),
+            score: 12_000_000,
+        },
+        HighScore {
+            label: Some("Second Place".to_string()),
+            short_label: Some("2nd".to_string()),
+            initials: "MJC".to_string(),
+            score: 11_000_000,
+        },
+        HighScore {
+            label: Some("Third Place".to_string()),
+            short_label: Some("3rd".to_string()),
+            initials: "J K".to_string(),
+            score: 10_000_000,
+        },
+        HighScore {
+            label: Some("Fourth Place".to_string()),
+            short_label: Some("4th".to_string()),
+            initials: "HEY".to_string(),
+            score: 9_000_000,
+        },
+    ]);
+    assert_eq!(expected, scores);
+
+    Ok(())
+}
