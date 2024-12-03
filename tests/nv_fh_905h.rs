e92ae70fad68526a94c19eae6877757df698a929
Francis De Brabandere <francisdb@gmail.com>
2024-12-03 12:13:08 +0100

    test: fh_l9

--- tests/nv_fh_905h.rs
+++ tests/nv_fh_905h.rs
@@ -3,15 +3,16 @@ use pretty_assertions::assert_eq;
 use std::io;
 use std::path::Path;
 
+// TODO create files for fh_905h.nv and fh_906h.nv
+
 #[test]
-#[ignore = "only fh_l9.nv is set up and incompatible"]
 fn test_funhouse() -> io::Result<()> {
-    let mut nvram = Nvram::open(Path::new("testdata/fh_905h.nv"))?.unwrap();
+    let mut nvram = Nvram::open(Path::new("testdata/fh_l9.nv"))?.unwrap();
 
     let last_game = nvram.read_last_game()?;
     let expected = Vec::from([
         LastGamePlayer {
-            score: 0,
+            score: 743_820,
             label: None,
         },
         LastGamePlayer {
@@ -32,28 +33,34 @@ fn test_funhouse() -> io::Result<()> {
     let scores = nvram.read_highscores()?;
     let expected = Vec::from([
         HighScore {
+            label: Some("Grand Champion".to_string()),
+            short_label: Some("GC".to_string()),
+            initials: "LED".to_string(),
+            score: 15_000_000,
+        },
+        HighScore {
             label: Some("First Place".to_string()),
             short_label: Some("1st".to_string()),
-            initials: "EJB".to_string(),
-            score: 4_000_000,
+            initials: "PML".to_string(),
+            score: 12_000_000,
         },
         HighScore {
             label: Some("Second Place".to_string()),
             short_label: Some("2nd".to_string()),
-            initials: "CLF".to_string(),
-            score: 3_800_000,
+            initials: "MJC".to_string(),
+            score: 11_000_000,
         },
         HighScore {
             label: Some("Third Place".to_string()),
             short_label: Some("3rd".to_string()),
-            initials: "JCD".to_string(),
-            score: 3_600_000,
+            initials: "J K".to_string(),
+            score: 10_000_000,
         },
         HighScore {
             label: Some("Fourth Place".to_string()),
             short_label: Some("4th".to_string()),
-            initials: "KWD".to_string(),
-            score: 3_400_000,
+            initials: "HEY".to_string(),
+            score: 9_000_000,
         },
     ]);
     assert_eq!(expected, scores);
