2b58050d58bad17834190537456e4daf16e8227f
Francis De Brabandere <francisdb@gmail.com>
2024-12-09 22:58:36 +0100

    feat: dip-switches

--- src/lib.rs
+++ src/lib.rs
@@ -103,6 +103,43 @@ impl Nvram {
         let mut file = OpenOptions::new().read(true).open(&self.nv_path)?;
         read_game_state(&mut file, &self.map)
     }
+
+    pub fn dip_switches_len(&self) -> usize {
+        // TODO get the number of dip switches from the map
+        // centaur
+        // 32 default switches
+        // 3 additional switches for the sound board reverb effect
+        32 + 3
+    }
+
+    /// Get the value of a dip switch
+    /// # Arguments
+    /// * `number` - The number of the dip switch to get, 1-based!
+    /// # Returns
+    /// * `Ok(true)` if the dip switch is ON
+    /// * `Ok(false)` if the dip switch is OFF
+    /// * `Err(io::Error)` if the dip switch number is out of range or an IO error occurred
+    pub fn get_dip_switch(&self, number: usize) -> io::Result<bool> {
+        let switch_count = self.dip_switches_len();
+        let mut file = OpenOptions::new().read(true).open(&self.nv_path)?;
+        get_dip_switch(&mut file, switch_count, number)
+    }
+
+    /// Set a dip switch to on or off
+    /// # Arguments
+    /// * `number` - The number of the dip switch to set, 1-based!
+    /// * `on` - `true` to set the dip switch to ON, `false` to set it to OFF
+    /// # Returns
+    /// * `Ok(())` if the dip switch was set successfully
+    /// * `Err(io::Error)` if the dip switch number is out of range or an IO error occurred
+    pub fn set_dip_switch(&self, number: usize, on: bool) -> io::Result<()> {
+        let switch_count = self.dip_switches_len();
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(&self.nv_path)?;
+        set_dip_switch(&mut file, switch_count, number, on)
+    }
 }
 
 trait ReadRoms {
@@ -628,6 +665,74 @@ fn update_all_checksum16<T: Read + Seek + Write>(
         .try_for_each(|cs| update_checksum16(&mut nvram_file, cs, *endian))
 }
 
+/// Number of bytes appended to the NVRAM for dip switches
+///
+/// PinMAME has a maximum of 10 banks with 8 switches each
+/// Somehow only 6 bytes are written to the nvram file
+/// https://github.com/vpinball/pinmame/blob/f14bbc89c48d0ecb0d44d4be7a694730cfbf24e1/src/wpc/core.c#L2303-L2309
+const DIP_SWITCH_BYTES: i64 = 6;
+
+pub fn get_dip_switch<T: Read + Seek>(
+    nvram_file: &mut T,
+    switch_count: usize,
+    number: usize,
+) -> io::Result<bool> {
+    validate_dip_switch_range(switch_count, number)?;
+    let index = number - 1;
+    let register = index / 8;
+    let bit = index % 8;
+    let mut buff = [0; 1];
+    nvram_file.seek(SeekFrom::End(-DIP_SWITCH_BYTES + register as i64))?;
+    nvram_file.read_exact(&mut buff)?;
+    Ok((buff[0] & (1 << bit)) != 0)
+}
+
+pub fn set_dip_switch<T: Read + Write + Seek>(
+    nvram_file: &mut T,
+    switch_count: usize,
+    number: usize,
+    on: bool,
+) -> io::Result<()> {
+    validate_dip_switch_range(switch_count, number)?;
+    let index = number - 1;
+    let register = index / 8;
+    let bit = index % 8;
+    // write single byte with value
+    let mut buff = [0; 1];
+    nvram_file.seek(SeekFrom::End(-DIP_SWITCH_BYTES + register as i64))?;
+    nvram_file.read_exact(&mut buff)?;
+    if on {
+        buff[0] |= 1 << bit;
+    } else {
+        buff[0] &= !(1 << bit);
+    }
+    nvram_file.seek(SeekFrom::End(-DIP_SWITCH_BYTES + register as i64))?;
+    nvram_file.write_all(&buff)
+}
+
+fn validate_dip_switch_range(switch_count: usize, number: usize) -> io::Result<()> {
+    const MAX_SWITCH_COUNT: i64 = DIP_SWITCH_BYTES * 8;
+    if switch_count > (MAX_SWITCH_COUNT) as usize {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            format!(
+                "Switch count {} out of range, expected 1-{}",
+                switch_count, MAX_SWITCH_COUNT
+            ),
+        ));
+    }
+    if number < 1 || number > switch_count {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            format!(
+                "Dip switch #{} out of range, expected 1-{}",
+                number, switch_count
+            ),
+        ));
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -667,6 +772,62 @@ mod tests {
     fn test_find_map() -> io::Result<()> {
         let map: Option<Value> = find_map(&"afm_113b".to_string())?;
         assert_eq!(true, map.is_some());
+        Ok(())
+    }
+
+    #[test]
+    fn test_dip_switches() -> io::Result<()> {
+        // cursor with 8 bytes, we always have 6 dip switch bytes
+        let switch_count = 32;
+        let mut cursor = io::Cursor::new(vec![0; 8]);
+        let switch1 = get_dip_switch(&mut cursor, switch_count, 1)?;
+        let switch2 = get_dip_switch(&mut cursor, switch_count, 32)?;
+
+        // switches are OFF
+        assert_eq!(false, switch1);
+        assert_eq!(false, switch2);
+        set_dip_switch(&mut cursor, switch_count, 1, true)?;
+        set_dip_switch(&mut cursor, switch_count, 32, true)?;
+
+        // switches are both ON
+        let switch1 = get_dip_switch(&mut cursor, switch_count, 1)?;
+        let switch2 = get_dip_switch(&mut cursor, switch_count, 32)?;
+        assert_eq!(true, switch1);
+        assert_eq!(true, switch2);
+
+        // the switch data should be written to the cursor
+        let mut buff = [0; 6];
+        cursor.seek(SeekFrom::End(-6))?;
+        cursor.read_exact(&mut buff)?;
+        assert_eq!([1, 0, 0, 128, 0, 0], buff);
+
+        // other data in the cursor should not be changed
+        let mut buff = [0; 2];
+        cursor.seek(SeekFrom::Start(0))?;
+        cursor.read_exact(&mut buff)?;
+        assert_eq!([0, 0], buff);
+        Ok(())
+    }
+
+    #[test]
+    fn test_dip_switch_out_of_range() -> io::Result<()> {
+        let mut cursor = io::Cursor::new(vec![0; 8]);
+        let result = get_dip_switch(&mut cursor, 16, 17);
+        assert!(matches!(
+            result,
+            Err(ref e) if e.kind() == io::ErrorKind::InvalidInput && e.to_string() == "Dip switch #17 out of range, expected 1-16"
+        ));
+        let result = set_dip_switch(&mut cursor, 16, 0, true);
+        assert!(matches!(
+            result,
+            Err(ref e) if e.kind() == io::ErrorKind::InvalidInput && e.to_string() == "Dip switch #0 out of range, expected 1-16"
+        ));
+        let result = set_dip_switch(&mut cursor, 8 * 6 + 1, 1, true);
+        println!("{:?}", result);
+        assert!(matches!(
+            result,
+            Err(ref e) if e.kind() == io::ErrorKind::InvalidInput && e.to_string() == "Switch count 49 out of range, expected 1-48"
+        ));
         Ok(())
     }
 }
