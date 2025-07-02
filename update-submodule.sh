d402603e4168d6eaff95fdb72777637d32951779
Francis De Brabandere <francisdb@gmail.com>
2025-07-02 16:13:15 +0200

    build: submodule master -> main

--- update-submodule.sh
+++ update-submodule.sh
@@ -2,5 +2,5 @@
 echo "Updating submodules"
 
 git submodule update
-git submodule foreach git checkout master
-git submodule foreach git pull origin master
+git submodule foreach git checkout main
+git submodule foreach git pull origin main
